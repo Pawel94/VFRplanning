c3a736ff424dbe2ebc7c9d61bd175098c7d0d8a0
paszewcz <pawel.szewczak@capgemini.com>
2022-11-10 18:57:58 +0100

    [19] - Updated navbar component

--- src/app/common/components/navbar/navbar.component.ts
+++ src/app/common/components/navbar/navbar.component.ts
@@ -1,10 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {WaypointManagerComponent} from "../../../route/components/waypoint-manager/waypoint-manager.component";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {WeatherManagerComponent} from "../../../weater/component/weather-manager/weather-manager.component";
-import {takeUntil} from "rxjs";
 import {CommonService} from "../../services/common.service";
-import {Airport} from "../../../map/model/modelForMaps";
 
 @Component({
   selector: 'vfr-navbar',
@@ -12,23 +9,15 @@ import {Airport} from "../../../map/model/modelForMaps";
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  private searchedOptions?: Airport[];
 
-  constructor( public modalService: NgbModal, public readonly common:CommonService) {
+  constructor(public modalService: NgbModal, public readonly common: CommonService) {
   }
 
   ngOnInit(): void {
-
-    this.common.getCitiesFromDB()
-        // takeUntil(this.unsubscribeSignal.asObservable()))
-      .subscribe(element => {
-        this.searchedOptions = element;
-      });
   }
 
   openModal() {
     const modalRef = this.modalService.open(WeatherManagerComponent);
-    modalRef.componentInstance.fromParent = this.searchedOptions;
     modalRef.result.then((result) => {
       console.log(result);
     }).catch((error) => {
