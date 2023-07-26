530224729403957b6f332265ca875d0cb8622b01
Pawel Szewczak <pawel.szewczak@capgemini.com>
2023-07-26 15:57:40 +0200

    [61] - Adjusted some styles in modal.

--- src/app/features/vfr-parameters/flight-parameters/component/flight-parameters/flight-parameters.component.ts
+++ src/app/features/vfr-parameters/flight-parameters/component/flight-parameters/flight-parameters.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {FlightParamsService} from "../../../../../shared/+state/flight-state/flight-params.service";
 import {FlightParams} from "../../../../../shared/model/flightParamsModel";
 import {Observable, Subject, takeUntil} from "rxjs";
@@ -7,36 +7,35 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {RouteService} from "../../../../../shared/+state/route-state/route.service";
 import {CommonService} from "../../../../../shared/services/communication/firebase-communication/common.service";
 import {PlaneTypeForSelect} from "../../../types/plane";
-import { NgIf, NgFor, AsyncPipe } from '@angular/common';
-import { TranslocoModule } from '@ngneat/transloco';
+import {AsyncPipe, NgFor, NgIf} from '@angular/common';
+import {TranslocoModule} from '@ngneat/transloco';
 
 @Component({
-    selector: 'vfr-flight-parameters',
-    templateUrl: './flight-parameters.component.html',
-    styleUrls: ['./flight-parameters.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [
-        TranslocoModule,
-        FormsModule,
-        ReactiveFormsModule,
-        NgIf,
-        NgFor,
-        AsyncPipe,
-    ],
+  selector: 'vfr-flight-parameters',
+  templateUrl: './flight-parameters.component.html',
+  styleUrls: ['./flight-parameters.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    TranslocoModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    NgFor,
+    AsyncPipe,
+  ],
 })
 export class FlightParametersComponent implements OnInit, OnDestroy {
 
 
   planesFromDateBase$: Observable<PlaneTypeForSelect[]> = this.dateBase.getPlanesFromDB();
-  private flightParamsData!: FlightParams
   private unsubscribe$ = new Subject<void>;
 
   flightPramsForm: FormGroup = new FormGroup<any>({
     planeVelocity: new FormControl<number | null>(0, {}),
     flightLevel: new FormControl<number | null>(1000, {}),
     planeFuel: new FormControl<number | null>(1000, {}),
-    planeType: new FormControl<PlaneTypeForSelect>({id: 1, name: ""})
+    planeTypeId: new FormControl<PlaneTypeForSelect>({id: 1, name: ""})
   })
 
   constructor(private readonly flightParams: FlightParamsService,
@@ -50,11 +49,16 @@ export class FlightParametersComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.flightParams.selectFlightParams$
       .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(params => this.flightParamsData = params)
+      .subscribe(params => {
+        this.flightPramsForm.setValue({
+          planeVelocity: params.planeVelocity,
+          flightLevel: params.flightLevel,
+          planeFuel: params.planeFuel,
+          planeTypeId: params.planeTypeId
+        })
+      })
 
-    this.flightPramsForm?.get("planeVelocity")?.setValue(this.flightParamsData.planeVelocity)
-    this.flightPramsForm?.get("flightLevel")?.setValue(this.flightParamsData.flightLevel)
-    this.flightPramsForm?.get("planeFuel")?.setValue(this.flightParamsData.planeFuel)
+
   }
 
   closeModal() {
