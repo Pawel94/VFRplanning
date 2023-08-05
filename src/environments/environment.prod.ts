2ecfcc51fe65386bf7a93767caf980ee38338201
Pawel Szewczak <pawel.szewczak@capgemini.com>
2023-08-05 10:18:15 +0200

    COMMIT PROD TEST

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase:{
+    apiKey: "",
+    authDomain: "vfr-flight.firebaseapp.com",
+    databaseURL: "https://vfr-flight-default-rtdb.firebaseio.com",
+    projectId: "vfr-flight",
+    storageBucket: "vfr-flight.appspot.com",
+    messagingSenderId: "",
+    appId: "1:581325165540:web:707850dbc756707c692df1",
+    measurementId: "G-NEGWC5Z4LM"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebase:{
+    apiKey: "",
+    authDomain: "vfr-flight.firebaseapp.com",
+    databaseURL: "https://vfr-flight-default-rtdb.firebaseio.com",
+    projectId: "vfr-flight",
+    storageBucket: "vfr-flight.appspot.com",
+    messagingSenderId: "",
+    appId: "1:581325165540:web:707850dbc756707c692df1",
+    measurementId: "G-NEGWC5Z4LM"
+  }
 };
 
 /*
